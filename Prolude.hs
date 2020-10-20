ab556cc626dd83bae7accd15a183069cfc3763c3
Sara Lichtenstein <sara@itpro.tv>
2020-10-20 07:09:33 -0600

    [ch51261] Prefer more polymorphic functions

--- Prolude.hs
+++ Prolude.hs
@@ -2,8 +2,8 @@ module Prolude
   ( module Prolude.Aeson
   , module Prolude.Core
   , module Prolude.Exception
+  , module Prolude.Foldable
   , module Prolude.Json
-  , module Prolude.Map
   , module Prolude.Maybe
   , module Prolude.Monad
   , module Prolude.MongoDB
@@ -15,8 +15,8 @@ where
 import Prolude.Aeson
 import Prolude.Core
 import Prolude.Exception
+import Prolude.Foldable
 import Prolude.Json
-import Prolude.Map
 import Prolude.Maybe
 import Prolude.Monad
 import Prolude.MongoDB
---
+++ Prolude/Foldable.hs
@@ -0,0 +1,10 @@
+module Prolude.Foldable
+    ( module Data.Foldable
+    , module Data.Map
+    , module GHC.Exts
+    )
+where
+
+import Data.Foldable (toList)
+import Data.Map (Map)
+import GHC.Exts (fromList)
