c69993f907e0e5e4b6dbab0569516befb216f381
Sara Lichtenstein <sara@itpro.tv>
2020-10-23 09:10:00 -0600

    [ch51968] Add Data.Set to Prolude

--- Prolude/Foldable.hs
+++ Prolude/Foldable.hs
@@ -1,10 +1,39 @@
 module Prolude.Foldable
     ( module Data.Foldable
     , module Data.Map
+    , module Data.Set
     , module GHC.Exts
+    , emptySet
+    , insertSet
+    , setMember
+    , setSingleton
+    , setUnion
+    , setUnions
     )
 where
 
 import Data.Foldable (toList)
 import Data.Map (Map)
+import Data.Set (Set)
 import GHC.Exts (fromList)
+import qualified Data.Bool as Bool
+import qualified Data.Foldable as Foldable
+import qualified Data.Ord as Ord
+
+emptySet :: Set a
+emptySet = Set.empty
+
+insertSet :: Ord.Ord a => a -> Set a -> Set a
+insertSet = Set.insert 
+
+setMember :: Ord.Ord a => a -> Set a -> Bool.Bool
+setMember = Set.member
+
+setSingleton :: a -> Set a
+setSingleton = Set.singleton
+
+setUnion :: Ord.Ord a => Set a -> Set a -> Set a
+setUnion = Set.union
+
+setUnions :: (Foldable.Foldable f, Ord.Ord a) => f (Set a) -> Set a
+setUnions = Set.unions
