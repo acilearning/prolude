cb4d0bfd29cf09cda12584183efaef5c07be34d0
Sara Lichtenstein <sara@itpro.tv>
2020-10-23 09:38:37 -0600

    [ch51968] Prefer qualified fns

--- Prolude/Foldable.hs
+++ Prolude/Foldable.hs
@@ -3,12 +3,6 @@ module Prolude.Foldable
     , module Data.Map
     , module Data.Set
     , module GHC.Exts
-    , emptySet
-    , insertSet
-    , setMember
-    , setSingleton
-    , setUnion
-    , setUnions
     )
 where
 
@@ -16,25 +10,3 @@ import Data.Foldable (toList)
 import Data.Map (Map)
 import Data.Set (Set)
 import GHC.Exts (fromList)
-import qualified Data.Bool as Bool
-import qualified Data.Foldable as Foldable
-import qualified Data.Ord as Ord
-import qualified Data.Set as Set
-
-emptySet :: Set a
-emptySet = Set.empty
-
-insertSet :: Ord.Ord a => a -> Set a -> Set a
-insertSet = Set.insert 
-
-setMember :: Ord.Ord a => a -> Set a -> Bool.Bool
-setMember = Set.member
-
-setSingleton :: a -> Set a
-setSingleton = Set.singleton
-
-setUnion :: Ord.Ord a => Set a -> Set a -> Set a
-setUnion = Set.union
-
-setUnions :: (Foldable.Foldable f, Ord.Ord a) => f (Set a) -> Set a
-setUnions = Set.unions
