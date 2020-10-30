209c5ac1ad1465a29a24824c463f0235069bcc34
Sara Lichtenstein <sara@itpro.tv>
2020-10-30 13:29:24 -0600

    [ch52460] Prefer renamed Csv functions

--- Prolude/Csv.hs
+++ Prolude/Csv.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ConstraintKinds #-}
+
 module Prolude.Csv
     ( module Data.Csv
     )
@@ -5,9 +7,15 @@ where
 
 import Data.Csv 
     ( DefaultOrdered
-    , FromField
-    , ToField
     , ToNamedRecord
-    , parseField
-    , toField
     )
+import qualified Data.Csv as Csv
+
+type CsvFromField = Csv.FromField
+type CsvToField = Csv.ToField
+
+parseCsvField :: CsvFromField a => Csv.Field -> Csv.Parser a
+parseCsvField = Csv.parseField
+
+toCsvField :: CsvToField a => a -> Csv.Field
+toCsvField = Csv.toField
