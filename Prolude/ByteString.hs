a420120bac5e3f6eb559b75e32c53459a356d518
Sara Lichtenstein <sara@itpro.tv>
2020-11-10 12:36:55 -0700

    [ch52930] Rename ByteString functions

--- Prolude/ByteString.hs
+++ Prolude/ByteString.hs
@@ -1,15 +1,21 @@
 module Prolude.ByteString
     ( module Data.ByteString
     , LazyByteString
-    , putLazyStr
+    , putLazyByteString
+    , writeByteStringToFile
     )
 where
 
-import Data.ByteString (ByteString, writeFile)
+import Data.ByteString (ByteString)
+import qualified Control.Monad.IO.Class as MonadIO
+import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as LBS
 import qualified System.IO as IO
 
 type LazyByteString = LBS.ByteString
 
-putLazyStr :: LazyByteString -> IO.IO ()
-putLazyStr = LBS.putStr
+putLazyByteString :: MonadIO.MonadIO m => LazyByteString -> m ()
+putLazyByteString = MonadIO.liftIO . LBS.putStr
+
+writeByteStringToFile :: MonadIO.MonadIO m => IO.FilePath -> ByteString -> m ()
+writeByteStringToFile path bs = MonadIO.liftIO $ BS.writeFile path bs
