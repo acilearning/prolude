1bf5a7bd83de56513c2587da6f5159c5816021f2
Zachary Churchill <zacharyachurchill@gmail.com>
2020-11-13 09:02:20 -0500

    remove (..) from types in Prim

--- Prolude/Prim.hs
+++ Prolude/Prim.hs
@@ -1,13 +1,13 @@
 module Prolude.Prim
-  ( Word8(..)
-  , Word16(..)
-  , Word32(..)
-  , Word64(..)
-  , Int8(..)
-  , Int16(..)
-  , Int32(..)
-  , Int64(..)
+  ( Word8
+  , Word16
+  , Word32
+  , Word64
+  , Int8
+  , Int16
+  , Int32
+  , Int64
   ) where
 
-import Data.Word (Word8(..), Word16(..), Word32(..), Word64(..))
-import Data.Int (Int8(..), Int16(..), Int32(..), Int64(..))
+import Data.Word (Word8, Word16, Word32, Word64)
+import Data.Int (Int8, Int16, Int32, Int64)
