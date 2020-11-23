64f4d50526596377b9a456b4db4784da4dcf3ff1
Sara Lichtenstein <sara@itpro.tv>
2020-11-23 13:52:04 -0700

    [ch54050] Add LazyText

--- Prolude/Text.hs
+++ Prolude/Text.hs
@@ -1,14 +1,26 @@
 module Prolude.Text
-  ( Text.Text
+  ( LazyText
+  , Text.Text
+  , lazyTextToString
+  , stringToLazyText
   , stringToText
   , textToString
   )
 where
 
 import qualified Data.Text as Text
+import qualified Data.Text.Lazy as LazyText
+
+type LazyText = LazyText.Text
 
 stringToText :: String -> Text.Text
 stringToText = Text.pack
 
 textToString :: Text.Text -> String
 textToString = Text.unpack
+
+stringToLazyText :: String -> LazyText
+stringToLazyText = LazyText.pack
+
+lazyTextToString :: LazyText -> String
+lazyTextToString = LazyText.unpack
