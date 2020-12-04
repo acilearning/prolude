b907443d7863c3337679789a0aa31bdd3ce70c00
Sara Lichtenstein <sara@itpro.tv>
2020-12-04 14:32:57 -0700

    [ch54633] Prefer unprefixed names

--- Prolude/Lens.hs
+++ Prolude/Lens.hs
@@ -1,14 +1,6 @@
 module Prolude.Lens 
-    ( setLens
-    , viewLens
+    ( module Control.Lens
     )
 where
 
-import qualified Control.Monad.Reader as Reader
-import qualified Control.Lens as Lens 
-
-setLens :: Lens.ASetter s t a b -> b -> s -> t
-setLens = Lens.set
-
-viewLens :: Reader.MonadReader s m => Lens.Getting a s a -> m a
-viewLens = Lens.view
+import Control.Lens (set, view)
