3181f2e31821a11c8f4b985f99ac1a0b0de1d1db
Sara Lichtenstein <sara@itpro.tv>
2020-12-16 08:38:44 -0700

    [ch55149] Document Swagger

--- Prolude/Swagger.hs
+++ Prolude/Swagger.hs
@@ -1,12 +1,20 @@
 {-# LANGUAGE ConstraintKinds #-}
 
 module Prolude.Swagger 
-    ( Swagger.SwaggerType(..)
+    ( -- * SwaggerType re-export
+      Swagger.SwaggerType(..)
+      -- * Alias types
+      -- $alias
     , SwaggerToSchema
     , SwaggerToParamSchema
+      -- * Functions
     , defaultDeclareNamedSchema
     )
 where
+
+{- $alias
+We created type aliases for Swagger.ToSchema and Swagger.ToParamSchema.
+-}
 
 import qualified Data.Swagger as Swagger
 import qualified Data.Swagger.Declare as Swagger
@@ -16,6 +24,7 @@ import qualified Data.Swagger.Internal.TypeShape as Swagger
 type SwaggerToParamSchema = Swagger.ToParamSchema
 type SwaggerToSchema = Swagger.ToSchema
 
+-- | Function to help with Swagger.ToSchema instances
 defaultDeclareNamedSchema
   :: Applicative f => (Swagger.Schema -> Swagger.Schema) -> proxy a -> f Swagger.NamedSchema
 defaultDeclareNamedSchema f = const . pure . Swagger.NamedSchema Nothing $ f mempty
