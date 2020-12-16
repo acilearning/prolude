239c9f68bd2d994277b5a00232df42e2d82b1758
Sara Lichtenstein <sara@itpro.tv>
2020-12-16 08:09:16 -0700

    [ch55149] Document Exception

--- Prolude/Exception.hs
+++ Prolude/Exception.hs
@@ -1,6 +1,8 @@
 module Prolude.Exception
-  ( module Control.Exception.Safe
+  ( -- * Exception re-exports
+    module Control.Exception.Safe
   , UnsafeException.AsyncException(..)
+    -- * Exception functions
   , catchIf
   , unsafeEvaluate
   , unsafeThrow
@@ -12,11 +14,14 @@ import Prolude.Core
 import qualified Control.Exception as UnsafeException
 import Control.Exception.Safe hiding (catchIO, throwM)
 
+-- | Function alias for Control.Exception.evaluate
 unsafeEvaluate :: a -> IO a
 unsafeEvaluate = UnsafeException.evaluate
 
+-- | Function alias for Control.Exception.throw
 unsafeThrow :: Exception e => e -> a
 unsafeThrow = UnsafeException.throw
 
+-- | Catch exception based on a predicate
 catchIf :: (MonadCatch m, Exception e) => (e -> Bool) -> m a -> (e -> m a) -> m a
 catchIf f a b = catch a $ \e -> if f e then b e else throw e
