437726cbde939d33daccdf00bc8cb368a77f038b
Sara Lichtenstein <sara@itpro.tv>
2020-12-16 09:02:17 -0700

    [ch55149] Document Uuid

--- Prolude/Uuid.hs
+++ Prolude/Uuid.hs
@@ -1,13 +1,15 @@
 module Prolude.Uuid 
-    ( Uuid
-    , randomUuid
-    , textToUuid
-    , uuidToText
-    , wordsToUuid
-    )
+  ( -- * Type alias
+    Uuid
+    -- * Functions
+  , randomUuid
+  , textToUuid
+  , uuidToText
+  , wordsToUuid
+  )
 where
 
-import Control.Monad.IO.Class (MonadIO(liftIO))
+import qualified Control.Monad.IO.Class as Monad
 import qualified Data.Maybe as Maybe
 import qualified Data.Text as Text
 import qualified Data.UUID as UUID
@@ -17,14 +19,18 @@ import qualified System.IO as IO
 
 type Uuid = UUID.UUID
 
+-- | Converts a Uuid to Text
 uuidToText :: Uuid -> Text.Text
 uuidToText = UUID.toText
 
+-- | Converts a Text to a Maybe Uuid
 textToUuid :: Text.Text -> Maybe.Maybe Uuid
 textToUuid = UUID.fromText
 
+-- | Creates a Uuid from Words
 wordsToUuid :: Word.Word32 -> Word.Word32 -> Word.Word32 -> Word.Word32 -> Uuid
 wordsToUuid = UUID.fromWords
 
-randomUuid :: MonadIO m => m Uuid
-randomUuid = liftIO UUID.nextRandom
+-- | Returns a randomUuid
+randomUuid :: Monad.MonadIO m => m Uuid
+randomUuid = Monad.liftIO UUID.nextRandom
