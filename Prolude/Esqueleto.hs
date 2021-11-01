ce73884b00b02731b11d4071f10c6ad4d2abdec1
Sara <sara@itpro.tv>
2021-11-01 11:47:59 -0600

    [sc-98309] Finish adding Esqueleto

--- Prolude/Esqueleto.hs
+++ Prolude/Esqueleto.hs
@@ -1,24 +1,71 @@
-module Prolude.Esqueleto where
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE FlexibleContexts #-}
 
+module Prolude.Esqueleto 
+  ( -- * Esqueleto re-exports
+    -- $esqueleto
+    module Database.Esqueleto.Experimental
+    -- * Alias type
+    -- $alias
+  , EsqueletoValue
+    -- * Alias functions
+    -- $functions
+  , esqueletoFrom
+  , esqueletoOn
+  , esqueletoSelect
+  , esqueletoVal
+  ) 
+where
+
+import qualified Control.Monad.IO.Class as IO
+import qualified Data.Bool as Bool
 import qualified Database.Esqueleto.Experimental as Esqueleto
+import qualified Database.Esqueleto.Experimental.From as Esqueleto.From
+import qualified Database.Esqueleto.Experimental.From.Join as Esqueleto.From.Join
+import qualified Database.Esqueleto.Internal.Internal as Esqueleto.Internal
 import Database.Esqueleto.Experimental 
   ( (=.)
   , (==.)
   , (^.)
-  , val
-  , where_
   , (&&.)
-  , InnerJoin(InnerJoin)
-  , in_
-  , on
-  , select
-  , valList
-  , unSingle
   , (<.)
   , (>=.)
+  , InnerJoin(InnerJoin)
+  , in_
+  , unSingle
+  , valList
+  , where_
   )
+
+{- $esqueleto
+These are our re-exports from Esqueleto. They include: (=.), (==.), (^.), (&&.), (<.), (>=.), InnerJoin(InnerJoin), in_, unSingle, valList, and where_  
+-}
+
+{- $alias
+We created a type alias for Esqueleto.Value.
+-}
+
+{- $functions
+We created aliases for from, on, select, and val.
+-}
 
 type EsqueletoValue = Esqueleto.Value
 
--- esqueletoFrom :: Esqueleto.ToFrom a a' => a -> Esqueleto.SqlQuery a'
--- esqueletoFrom = Esqueleto.from
+esqueletoFrom :: Esqueleto.From.ToFrom a a' => a -> Esqueleto.SqlQuery a'
+esqueletoFrom = Esqueleto.from
+
+esqueletoSelect 
+  :: (Esqueleto.Internal.SqlSelect a r, IO.MonadIO m) 
+  => Esqueleto.SqlQuery a 
+  -> Esqueleto.SqlReadT m [r]
+esqueletoSelect = Esqueleto.select
+
+esqueletoOn 
+  :: Esqueleto.From.Join.ValidOnClause a
+  => a 
+  -> (b -> Esqueleto.SqlExpr (EsqueletoValue Bool.Bool)) 
+  -> (a, b -> Esqueleto.SqlExpr (EsqueletoValue Bool.Bool))
+esqueletoOn = Esqueleto.on
+
+esqueletoVal :: Esqueleto.PersistField typ => typ -> Esqueleto.SqlExpr (EsqueletoValue typ)
+esqueletoVal = Esqueleto.val
